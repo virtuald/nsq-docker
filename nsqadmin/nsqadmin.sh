0c56245be13d06b82f829dc5fadf08add1226fab
Dustin Spicuzza <dustin@virtualroadside.com>
2014-09-08 16:56:51 -0400

    nsqadmin doesn't like http at the beginning

--- nsqadmin/nsqadmin.sh
+++ nsqadmin/nsqadmin.sh
@@ -10,7 +10,7 @@ if [ "$NSQLOOKUPD_PORT" != "" ]; then
     # we extract the ip from the port because of the port is incorrect 
     # when using ambassadord, otherwise we could just use it directly
     LOOKUPD_IP=`echo $NSQLOOKUPD_PORT | sed 's/tcp:\/\/\(.*\):.*/\1/'`
-    LOOKUPD_ARGS=-lookupd-http-address=http://$LOOKUPD_IP:4161
+    LOOKUPD_ARGS=-lookupd-http-address=$LOOKUPD_IP:4161
 fi
 
 exec /usr/bin/nsqadmin $LOOKUPD_ARGS $@
