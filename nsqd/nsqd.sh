b1ddfce0820ddb80972b23e5b277d5a83129af72
Dustin Spicuzza <dustin@virtualroadside.com>
2016-04-13 14:45:57 -0400

    Update NSQ to 0.3.7

--- nsqd/nsqd.sh
+++ nsqd/nsqd.sh
@@ -7,10 +7,7 @@
 # The second part must ALWAYS be nsqlookupd. 
 
 if [ "$NSQLOOKUPD_PORT" != "" ]; then
-    # we extract the ip from the port because of the port is incorrect 
-    # when using ambassadord, otherwise we could just use it directly
-    LOOKUPD_IP=`echo $NSQLOOKUPD_PORT | sed 's/tcp:\/\/\(.*\):.*/\1/'`
-    LOOKUPD_ARGS=-lookupd-tcp-address=$LOOKUPD_IP:4160
+    LOOKUPD_ARGS=-lookupd-tcp-address=nsqlookupd:4160
 fi
 
 #
